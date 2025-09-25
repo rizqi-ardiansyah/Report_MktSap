ee561f13e17afda17201351ec5f3a9d8a5e8c01a
Muhammad Rizqi Ardiansyah <muh.rizqiardian@gmail.com>
2025-09-25 16:41:39 +0700

    #5_ChangeLayoutAndAddHeaderAndFooter

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -7,82 +7,89 @@
         <img src="../assets/logo.png" alt="Company Logo" />
       </div>
       <div class="header-info">
-        <p><strong>TITLE</strong> : BUDGET PEMBELIAN MATERIAL & CKD SEPTEMBER 2025</p>
-        <p><strong>PROJECT</strong> : SU2ID & KS</p>
-        <p><strong>CUSTOMER</strong> : PT HMMI</p>
+        <div class="row">
+          <span class="label bold">TITLE</span>
+          <span class="value bold">: BUDGET PEMBELIAN MATERIAL & CKD SEPTEMBER 2025</span>
+        </div>
+        <div class="row">
+          <span class="label bold">PROJECT</span>
+          <span class="value bold">: SU2ID & KS</span>
+        </div>
+        <div class="row">
+          <span class="label bold">CUSTOMER</span>
+          <span class="value bold">: PT HMMI</span>
+        </div>
       </div>
-      <div class="rev-date">
-        <p><strong>REV.</strong> 00</p>
-        <p>24 September 2025</p>
+      <div class="rev">
+        <p style="font-weight: bold;">REV. 00</p>
       </div>
     </div>
 
 
     <!-- <h2 class="text-xl font-bold mb-2">Data Budget</h2> -->
-    <table class="report-table" border="1" id="budget-table">
+    <table class="report-table" id="budget-table">
       <thead>
         <tr>
-          <th style="background-color: white;" colspan="23"></th>
-          <th style="background-color: white;">22 Sep 2025</th>
+          <th style="background-color: white; border: none;" colspan="21"></th>
+          <th colspan="3" style="background-color: white; font-style: italic;">{{ formattedDate }}</th>
         </tr>
         <tr>
           <th rowspan="4" style="width:10px; text-align:center;">No</th>
           <th rowspan="4">Model</th>
           <th rowspan="4" colspan="2">Material/CKD</th>
-          <th rowspan="4">Σ Item V <br>to V / <br>Gpart</th>
+          <th rowspan="4">Σ Item <br> V to V / <br>Gpart</th>
           <!-- Bulan -->
-          <th colspan="19" style="font-size: 14px; font-weight: bold;">Budget Material & CKD</th>
+          <th colspan="19" style="font-size: 14px; font-weight: bold;">BUDGET MATERIAL & CKD</th>
         </tr>
         <tr>
-          <th colspan="3">Aug-25</th>
-          <th colspan="3">Aug-25</th>
-          <th rowspan="3">Diff</th>
+          <th colspan="6">Aug-25</th>
+          <th rowspan="3">DIFF</th>
           <th colspan="3">Sep-25</th>
           <th colspan="3">Oct-25</th>
           <th colspan="3">Nov-25</th>
           <th colspan="3">Dec-25</th>
         </tr>
         <tr>
-          <th colspan="3">Forecast</th>
-          <th colspan="3">Actual</th>
-          <th colspan="3">Forecast</th>
-          <th colspan="3">Forecast</th>
-          <th colspan="3">Forecast</th>
-          <th colspan="3">Forecast</th>
+          <th colspan="3">FORECAST</th>
+          <th colspan="3">ACTUAL</th>
+          <th colspan="3">FORECAST</th>
+          <th colspan="3">FORECAST</th>
+          <th colspan="3">FORECAST</th>
+          <th colspan="3">FORECAST</th>
         </tr>
         <tr>
           <!-- Sub kolom -->
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
-          <th>PCS</th>
-          <th>KG</th>
-          <th>Amount</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
+          <th class="italic">PCS</th>
+          <th class="italic">KG</th>
+          <th class="italic">AMOUNT <br>(x1000)</th>
         </tr>
       </thead>
       <tbody>
         <template v-for="(row, index) in budget_mat" :key="row.id">
           <!-- Jika Material biasa -->
           <tr v-if="row.name === 'Material'">
-            <td v-if="shouldShowNo(index)" :rowspan="rowspanNo(row.nokol)">
+            <td v-if="shouldShowNo(index)" :rowspan="rowspanNo(row.nokol)" class="bold">
               {{ row.nokol }}
             </td>
-            <td v-if="shouldShowModel(index)" :rowspan="rowspanModel(row.nokol, row.model)">
+            <td v-if="shouldShowModel(index)" :rowspan="rowspanModel(row.nokol, row.model)" class="bold">
               {{ row.model }}
             </td>
-            <td colspan="2">Material</td>
+            <td colspan="2" class="bold">Material</td>
             <!-- Σ Item (merge) -->
             <td v-if="shouldShowTotalItem(index)" :rowspan="3">
               {{ row.total_item }}
@@ -93,13 +100,15 @@
             <td>{{ formatNumber(row.n0_pcs_act) }}</td>
             <td>{{ formatNumber(row.n0_kg_act) }}</td>
             <td>{{ formatNumber(row.n0_amount_act) }}</td>
-            <td>{{ formatNumber(row.difference) }}</td>
+
+            <td style="color: red;">{{ formatNumber(row.difference) }}%</td>
+
             <td>{{ formatNumber(row.n_pcs_forecast) }}</td>
             <td>{{ formatNumber(row.n_kg_forecast) }}</td>
             <td>{{ formatNumber(row.n_amount_forecast) }}</td>
             <td>{{ formatNumber(row.n1_pcs_forecast) }}</td>
             <td>{{ formatNumber(row.n1_kg_forecast) }}</td>
-            <td>{{ formatNumber(row.n1_amount_forecast)}}</td>
+            <td>{{ formatNumber(row.n1_amount_forecast) }}</td>
             <td>{{ formatNumber(row.n2_pcs_forecast) }}</td>
             <td>{{ formatNumber(row.n2_kg_forecast) }}</td>
             <td>{{ formatNumber(row.n2_amount_forecast) }}</td>
@@ -118,10 +127,10 @@
               <td v-if="shouldShowModel(index)" :rowspan="2">
                 {{ row.model }}
               </td>
-              <td rowspan="2">CKD</td>
+              <td rowspan="2" class="bold">CKD</td>
 
               <!-- Baris CKD PCS -->
-              <td>PCS</td>
+              <td class="bold">PCS</td>
 
               <!-- Merge kolom CKD PCS dan Transport -->
               <td :rowspan="2">{{ sumCKD(row.nokol, row.model, 'n0_pcs_forecast') }}</td>
@@ -143,22 +152,22 @@
                 {{ formatNumber(row.n0_kg_act) }}
               </td>
               <td v-else>
-                {{ formatNumber(row.n0_kg_act)}}
+                {{ formatNumber(row.n0_kg_act) }}
               </td>
               <td>
                 {{ formatNumber(row.n0_amount_act) }}
               </td>
 
-              <td>{{ formatNumber(row.difference) }}</td>
+              <td style="color: red;">{{ formatNumber(row.difference) }}%</td>
 
 
               <td :rowspan="2">{{ sumCKD(row.nokol, row.model, 'n_pcs_forecast') }}</td>
 
               <td v-if="isSameCKD(row.nokol, row.model, 'n_kg_forecast')" :rowspan="2">
-                {{formatNumber( row.n_kg_forecast) }}
+                {{ formatNumber(row.n_kg_forecast) }}
               </td>
               <td v-else>
-                {{formatNumber( row.n_kg_forecast) }}
+                {{ formatNumber(row.n_kg_forecast) }}
               </td>
               <td>
                 {{ formatNumber(row.n_amount_forecast) }}
@@ -204,7 +213,7 @@
           <template v-else-if="row.name === 'CKD Transport'">
             <!-- Baris CKD - Transport -->
             <tr>
-              <td>Transport</td>
+              <td class="bold">Transport</td>
 
               <td v-if="!isSameCKD(row.nokol, row.model, 'n0_kg_forecast')">
                 {{ formatNumber(row.n0_kg_forecast) }}
@@ -220,7 +229,7 @@
                 {{ formatNumber(row.n0_amount_act_) }}
               </td>
 
-              <td>{{ row.difference }}</td>
+              <td style="color: red">{{ row.difference }}%</td>
 
               <td v-if="!isSameCKD(row.nokol, row.model, 'n_kg_forecast')">
                 {{ formatNumber(row.n_kg_forecast) }}
@@ -230,7 +239,7 @@
               </td>
 
               <td v-if="!isSameCKD(row.nokol, row.model, 'n1_kg_forecast')">
-                {{ formatNumber(row.n1_kg_forecast)}}
+                {{ formatNumber(row.n1_kg_forecast) }}
               </td>
               <td v-if="!isSameCKD(row.nokol, row.model, 'n1_amount_forecast')">
                 {{ formatNumber(row.n1_amount_forecast) }}
@@ -254,34 +263,90 @@
           </template>
         </template>
         <tr>
-          <td colspan="4">Total</td>
-          <td>{{ totalColumns.total_item }}</td>
-          <td>{{ totalColumns.n0_pcs_forecast }}</td>
-          <td>{{ totalColumns.n0_kg_forecast }}</td>
-          <td>{{ totalColumns.n0_amount_forecast }}</td>
-          <td>{{ totalColumns.n0_pcs_act }}</td>
-          <td>{{ totalColumns.n0_kg_act }}</td>
-          <td>{{ totalColumns.n0_amount_act }}</td>
-          <td>{{ formatNumber(totalDiff)}}</td> <!-- kolom Diff bisa dikosongi atau hitung sendiri -->
-          <td>{{ totalColumns.n_pcs_forecast }}</td>
-          <td>{{ totalColumns.n_kg_forecast }}</td>
-          <td>{{ totalColumns.n_amount_forecast }}</td>
-          <td>{{ totalColumns.n1_pcs_forecast }}</td>
-          <td>{{ totalColumns.n1_kg_forecast }}</td>
-          <td>{{ totalColumns.n1_amount_forecast }}</td>
-          <td>{{ totalColumns.n2_pcs_forecast }}</td>
-          <td>{{ totalColumns.n2_kg_forecast }}</td>
-          <td>{{ totalColumns.n2_amount_forecast }}</td>
-          <td>{{ totalColumns.n3_pcs_forecast }}</td>
-          <td>{{ totalColumns.n3_kg_forecast }}</td>
-          <td>{{ totalColumns.n3_amount_forecast }}</td>
+          <td class="bold" colspan="4">Total</td>
+          <td class="bold">{{ totalColumns.total_item }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_pcs_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_kg_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_amount_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_pcs_act) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_kg_act) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n0_amount_act) }}</td>
+          <td style="color: red; font-weight: bold;">{{ totalDiff.toFixed(2) }}%</td>
+          <td class="bold">{{ formatNumber(totalColumns.n_pcs_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n_kg_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n_amount_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n1_pcs_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n1_kg_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n1_amount_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n2_pcs_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n2_kg_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n2_amount_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n3_pcs_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n3_kg_forecast) }}</td>
+          <td class="bold">{{ formatNumber(totalColumns.n3_amount_forecast) }}</td>
         </tr>
       </tbody>
     </table>
-      <br><br>
-     <button @click="exportExcel" class="mb-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+    <!-- <br><br>
+    <button @click="exportExcel" class="mb-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
       Download Excel
-    </button><br><br>
+    </button><br><br> -->
+
+    <!-- Notes Section -->
+    <div class="notes-section">
+      <div class="notes-content">
+      <p><strong>Note:</strong></p>
+      <ul>
+        <li>Budget Material Sep 2025 menggunakan Material Price periode Jul - Sep 2025.</li>
+        <li>Budget Sep 2025 sudah termasuk Spare Part.</li>
+        <li>Budget Transport merupakan Budget Transport CKD Import.</li>
+        <li>Diff. Budget MDFO vs Act Aug’25 terabsorb pada qty budget Sep’25.</li>
+        <li>Qty 1st Week Sep’25 masuk ke dalam budget Aug-25.</li>
+      </ul>
+      </div>
+    </div>
+
+    <!-- Signature Section -->
+    <div class="signature-section">
+
+
+
+      <table class="signature-table">
+        <thead>
+          <tr>
+            <th>Menyetujui</th>
+            <th>Mengetahui</th>
+            <th colspan="2">Diperiksa</th>
+            <th>Dibuat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="height: 60px;"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <!-- <td></td> -->
+            <td class="bold">Rudy B. Darmawan</td>
+            <td class="bold">Aditya Herliawan</td>
+            <td class="bold">Erick Maryansyah</td>
+            <td class="bold">Imam Khudori</td>
+            <td class="bold">Gema Ramadhan P.</td>
+          </tr>
+          <tr>
+            <td><span class="role">Deputy Executive Officer<br>Commercial</span></td>
+            <td><span class="role">Dept. Head<br>Marketing</span></td>
+            <td><span class="role">Asst. Dept. Head<br>Marketing</span></td>
+            <td><span class="role">Asst. Dept. Head<br>Marketing</span></td>
+            <td><span class="role">Staff<br>Marketing</span></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 </template>
 
@@ -292,7 +357,7 @@ import * as XLSX from "xlsx";
 export default {
   data() {
     return {
-      budget_mat: []
+      budget_mat: [],
     };
   },
   mounted() {
@@ -338,14 +403,16 @@ export default {
 
       // jumlahkan field dari PCS + Transport
       return group.reduce((total, r) => {
-        return total + (parseFloat(r[field]) || 0);
+        return this.formatNumber(total + (parseFloat(r[field]) || 0));
       }, 0);
     },
 
     formatNumber(value) {
-      if (value == null || value === "") return "";
-      const num = Number(value);
-      return num.toFixed(3).replace(/\.?0+$/, "");
+      if (!value) return "0";
+      // Ensure it's a number
+      value = Number(value);
+      // Convert to string and insert dot every 3 digits
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
     sumMaterial(row) {
       const pcs = parseFloat(row.total_item) || 0;
@@ -362,6 +429,15 @@ export default {
   },
 
   computed: {
+    formattedDate() {
+      const today = new Date();
+      return today.toLocaleDateString("en-GB", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+      });
+    },
+
     totalColumns() {
       const totals = {
         total_item: 0,
@@ -392,19 +468,20 @@ export default {
       });
 
       for (let key in totals) {
-      totals[key] = Math.round(totals[key]); // bisa ganti ke Math.floor / Math.ceil sesuai kebutuhan
-    }
+        totals[key] = Math.round(totals[key]); // bisa ganti ke Math.floor / Math.ceil sesuai kebutuhan
+      }
       return totals;
     },
 
-    totalDiff(){
-      const {n0_amount_act, n0_amount_forecast} = this.totalColumns;
+    totalDiff() {
+      const { n0_amount_act, n0_amount_forecast } = this.totalColumns;
 
-      if(!n0_amount_forecast){
+      if (!n0_amount_forecast) {
         return 0;
       }
 
-      return (n0_amount_act-n0_amount_forecast)/n0_amount_act;
+      const diff = (n0_amount_act - n0_amount_forecast) / n0_amount_act;
+      return diff * 100;
     }
   }
 
